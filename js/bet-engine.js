4779f4c721ee3ffb0dbe35c943af2a9f54f9ed27
dataapk <onlinedatabase.expert@gmail.com>
2026-06-13 19:57:55 +0600

    Update bet-engine.js

--- js/bet-engine.js
+++ js/bet-engine.js
@@ -1,1 +1,12 @@
+const chipSound = document.getElementById("chipSound");
 
+document.querySelectorAll(".chip").forEach(chip => {
+    chip.addEventListener("click", () => {
+
+        if (chipSound) {
+            chipSound.currentTime = 0;
+            chipSound.play();
+        }
+
+    });
+});
