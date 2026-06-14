e05da5ea4c6dd47cbcf5c977b5cb017853b7662e
dataapk <onlinedatabase.expert@gmail.com>
2026-06-14 15:01:38 +0600

    Update bet-engine.js

--- js/bet-engine.js
+++ js/bet-engine.js
@@ -36,6 +36,56 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("🎰 Chip System Ready");
 
 });
+// START: PREMIUM SYMBOL ENGINE
+
+const symbolBoxes =
+document.querySelectorAll(
+    ".symbol-box"
+);
+
+const tableSound =
+new Audio(
+    "assets/table.mp3"
+);
+
+tableSound.volume = 0.35;
+
+let selectedSymbol = null;
+
+symbolBoxes.forEach(box => {
+
+    box.addEventListener(
+        "click",
+        () => {
+
+            tableSound.currentTime = 0;
+
+            tableSound.play();
+
+            symbolBoxes.forEach(item => {
+                item.classList.remove(
+                    "active"
+                );
+            });
+
+            box.classList.add(
+                "active"
+            );
+
+            selectedSymbol =
+            box.dataset.symbol;
+
+            console.log(
+                "Selected Symbol:",
+                selectedSymbol
+            );
+
+        }
+    );
+
+});
+
+// END: PREMIUM SYMBOL ENGINE
 
 // ===============================
 // 🪙 CHIP SYSTEM
