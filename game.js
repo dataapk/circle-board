d854ad6e506b08c01b84d8377fd1c3bd45e2d88b
dataapk <onlinedatabase.expert@gmail.com>
2026-07-07 23:51:47 +0600

    Update game.js

--- game.js
+++ game.js
@@ -35,10 +35,7 @@ const chips = document.querySelectorAll(
 const symbolBoxes = document.querySelectorAll(".symbol-box");
 
 const pointer = document.getElementById("pointer");
-const profile = await getUserProfile();
 
-document.getElementById("playerIdValue").textContent =
-    profile.user_id;
 // =========================
 // MENU SYSTEM
 // =========================
